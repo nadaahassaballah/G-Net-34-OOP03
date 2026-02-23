23d7f68bf367d479d610a12968d6cf3e8f54660c
Nada Hamdy <nadaahasabbalh@gmail.com>
2026-02-23 14:36:46 +0200

    Add project files.

---
+++ G-Net-34-OOP02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_Net_34_OOP02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
